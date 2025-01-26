5efba9a3d22e5d845cf64e085cf77ca6edf4b932
Revers <victorpg182006@gmail.com>
2025-01-26 18:51:24 +0100

    guardado

---
+++ JavaScript/ejercicios/borrar_btn.js
@@ -0,0 +1,8 @@
+const borrar = document.getElementById('borrar');
+const divtexto = document.getElementsByClassName('texto')
+
+function borrar () {
+    divtexto +=   ; 
+}
+
+document.getElementById('borrar').addEventListener('click', borrar);
